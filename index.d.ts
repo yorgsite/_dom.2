98e5f70ceacd3ffd8a3ffc90ca4c422e5df77ef0
yorgsite <gillesbdlk@gmail.com>
2022-12-24 05:05:51 +0100

    remove temporaly typescript adapter

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-
-
-interface _domIt {
-	(tagName:string,datas:object|Array,childs:Array,nameSpace:string|undefined):HTMLElement;
-	model: function(tagName,construct,cssRules,shadowed):void;
-}
-
-declare const _dom:_domIt;
-export default _dom;
